f9f0f6e0f8b9f504f3f75064a3a795816cb62d0c
yogi <yogimyla138@gmail.com>
2024-05-23 15:54:42 +0000

    Rock Paper Scissors 2

---
+++ src/components/GameOptions/styledComponents.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const OptionImage = styled.img`
+  width: 170px;
+  height: 170px;
+`
+
+export const OptionListItem = styled.li`
+  listy-style-type: none;
+  display: flex;
+`
+
+export const GameOptionButton = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`
